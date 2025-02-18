94a414be764562c3027bf0de3de1682a598a1b21
Pamela Kadja <kadjapamelaoliveira@gmail.com>
2025-02-17 21:14:06 -0300

    Add files via upload

---
+++ Questao11.py
@@ -0,0 +1,9 @@
+num1int= int (input ("Digite o 1 numero inteiro :"))
+num2int= int (input ("digite o 2 numero inteiro:"))
+numreal = float (input ("Digite o numero real :"))
+soma1 = (num1int*2)*(num2int/2)
+soma2 = (num1int*3) + numreal
+soma3 = numreal**3
+print (F"O resultado da a é: {soma1}")
+print (f"O resultado da b é : {soma2}")
+print (f"O resultado da c é : {soma3}")
