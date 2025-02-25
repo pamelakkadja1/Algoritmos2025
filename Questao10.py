3fd673c16243d36c61c7e5845abfc25d4d5ae3cc
Pamela Kadja <kadjapamelaoliveira@gmail.com>
2025-02-25 19:15:52 -0300

    Add files via upload

---
+++ Questao10.py
@@ -0,0 +1,3 @@
+C= float (input ("Digite o valor em Celsius :"))
+F= (C*9)/5+32
+print (f"O valor em Farenhait é : {F}")
---
+++ Questão9.py
@@ -0,0 +1,4 @@
+F = float (input("Digite os graus em Farenhait: "))
+C = 5*((F-32))/9
+print (f"Convertido em graus Celcius fica :{C}")
+
