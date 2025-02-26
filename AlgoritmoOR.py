5b3fdabc9e51eba7aad62ff4949e07e7be411ece
Pamela Kadja <kadjapamelaoliveira@gmail.com>
2025-02-25 21:22:33 -0300

    Add files via upload

---
+++ AlgoritmoOR.py
@@ -0,0 +1,5 @@
+num=float(input("Digite um numero :"))
+if(num>=12 or num<0 or num==100 or num==200):
+    print("Alou Brasil")
+else:
+    print("Não atende as condiçoes")
---
+++ Or.py
@@ -0,0 +1,5 @@
+num= int(input("Digite um numero:"))
+if(num>10 or num<5 or num==5):
+    print("Esse numero é maior que 10 ou menor que 5 ou igual a 4")
+else:
+    print("Não entrou no if")
